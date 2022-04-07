022d0f35fb9c41237c9b0c83806a7febab0c6738
quangthai206 <kysubon@teneocto.io>
2022-04-07 16:47:57 +0700

    Initial Commit

---
+++ SumsUpLogin/SumsUpLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumsUpLoginApp.swift
+//  SumsUpLogin
+//
+//  Created by quangthai206 on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SumsUpLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
